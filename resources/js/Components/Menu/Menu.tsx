62541f529f35ce0ab8c43cc5bf3822f9f9afaf91
Asanka nawarathna <nmsa.nawarathna#@gmail.com>
2026-03-27 15:40:12 +0530

    Refactor navigation logic in Menu component and update NewDashboardController for impersonation handling

--- resources/js/Components/Menu/Menu.tsx
+++ resources/js/Components/Menu/Menu.tsx
@@ -6,15 +6,17 @@ import {Link, usePage} from "@inertiajs/react";
 import {PageProps} from "@/types";
 import {FingerPrintIcon} from "@heroicons/react/24/solid";
 
-const navigation = [
+const baseNavigation = [
     { name: 'Dashboard', href: '/dashboard' },
-    { name: 'New Dashboard', href: '/new-dashboard' },
     { name: 'Profile', href: '/account' },
 ]
 
-
 export default function Menu() {
     const { auth, is_impersonating } = usePage<PageProps>().props;
+
+    const navigation = is_impersonating
+        ? [{ name: 'Dashboard', href: '/dashboard' }, { name: 'New Dashboard', href: '/new-dashboard' }, { name: 'Profile', href: '/account' }]
+        : baseNavigation;
 
 
     const isLoggedIn = !!auth.user;
--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,6 @@ export default defineConfig({
             input: 'resources/js/app.tsx',
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
-            valetTls: 'modelwise.test',
         }),
         react(),
     ],
